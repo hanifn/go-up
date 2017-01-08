0f188b67a9af91f462247d28eee6deba96326e99
Hanif Norman <hanifnorman@gmail.com>
2017-01-08 14:17:19 +0800

    added test file for file controller
    again,  using go tests to generate the tests but will probably need to use the net/http/httptest package with these since we're dealing with http requests

---
+++ controllers/file_test.go
@@ -0,0 +1,78 @@
+package controllers
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestGetFiles(t *testing.T) {
+	type args struct {
+		w   http.ResponseWriter
+		req *http.Request
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			GetFiles(tt.args.w, tt.args.req)
+		})
+	}
+}
+
+func TestUpload(t *testing.T) {
+	type args struct {
+		w   http.ResponseWriter
+		req *http.Request
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			Upload(tt.args.w, tt.args.req)
+		})
+	}
+}
+
+func TestDownload(t *testing.T) {
+	type args struct {
+		w   http.ResponseWriter
+		req *http.Request
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			Download(tt.args.w, tt.args.req)
+		})
+	}
+}
+
+func TestDelete(t *testing.T) {
+	type args struct {
+		w   http.ResponseWriter
+		req *http.Request
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			Delete(tt.args.w, tt.args.req)
+		})
+	}
+}
